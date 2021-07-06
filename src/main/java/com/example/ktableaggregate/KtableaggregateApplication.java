ea9c103b096eb0f5c81e5845827dcf9ed468f752
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-06 09:31:10 -0300

    initial commit

---
+++ src/main/java/com/example/ktableaggregate/KtableaggregateApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktableaggregate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtableaggregateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtableaggregateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktableaggregate/KtableaggregateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktableaggregate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtableaggregateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
