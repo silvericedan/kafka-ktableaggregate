f3b955562522bfcb968bdf628560ffef0c13ddca
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-06 12:54:14 -0300

    fixed script schema namespace

--- practice-resources/kafka-scripts.sh
+++ practice-resources/kafka-scripts.sh
@@ -9,7 +9,7 @@ confluent local services start
 kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic employees-topic
 
 kafka-avro-console-producer --broker-list localhost:9092 --topic employees-topic \
---property value.schema='{"namespace": "guru.learningjournal.examples.kafka.model","type": "record","name": "Employee","fields": [{"name": "id","type": "string"},{"name": "name","type": "string"},{"name": "department","type": "string"},{"name": "salary","type":"int"}]}'
+--property value.schema='{"namespace": "com.example.ktableaggregate.model","type": "record","name": "Employee","fields": [{"name": "id","type": "string"},{"name": "name","type": "string"},{"name": "department","type": "string"},{"name": "salary","type":"int"}]}'
 
 
 
